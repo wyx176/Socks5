8b87ba561fc2d0ad6310cb641233dd9b08cf77df
17DL <32320103+wyx176@users.noreply.github.com>
2018-05-04 21:35:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
+service ss5 stop
+
 rm -rf ss5-3.8.9
 rm -rf /etc/opt/ss5
+rm -f /usr/local/bin/s5
 
 echo ""
 echo "安装Socks5所依赖的组件,请稍等..."
