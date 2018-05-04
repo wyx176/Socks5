577de24066a4508cb5ce2fb0465032e2fffea601
17DL <32320103+wyx176@users.noreply.github.com>
2018-05-04 22:59:26 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -41,6 +41,10 @@ if [[ $choice == 4 ]];then
 fi
 
 if [[ $choice == 5 ]];then
+  echo "确定要卸载Socks5服务吗？"
+  echo "输入‘886’开始卸载,其它则取消卸载！"
+  read -p "请输入： " T
+  if [[ ! $T ==886 ]]; then
 	service ss5 stop
 	rm -rf ss5-3.8.9
 	rm -rf /etc/opt/ss5
@@ -48,4 +52,7 @@ if [[ $choice == 5 ]];then
 	clear
 	echo "Socks5服务卸载完毕！"
 	exit 0
+	
+	else
+	bash /etc/opt/ss5/service.sh
 fi
