b9ce7bcc9db55bb1450623dfdb334ca337923eff
17DL <32320103+wyx176@users.noreply.github.com>
2019-01-20 15:36:23 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Set PATH
+serviceFile=/etc/opt/ss5/service.sh
 echo ""
 echo "1.启动"
 echo "2.停止"
@@ -24,25 +25,25 @@ fi
 if [[ $choice == 1 ]];then
 	clear
 	service ss5 start
-	bash /etc/opt/ss5/service.sh
+	bash $serviceFile
 fi
 
 if [[ $choice == 2 ]];then
 	clear
 	service ss5 stop
-	bash /etc/opt/ss5/service.sh
+	bash $serviceFile
 fi
 
 if [[ $choice == 3 ]];then
 	clear
 	service ss5 restart
-	bash /etc/opt/ss5/service.sh
+	bash $serviceFile
 fi
 
 if [[ $choice == 4 ]];then
 	clear
 	service ss5 status
-	bash /etc/opt/ss5/service.sh
+	bash $serviceFile
 fi
 
 if [[ $choice == 5 ]];then
@@ -51,13 +52,22 @@ if [[ $choice == 5 ]];then
 	read -p "输入886开始卸载,其它则取消： " c
 	if [[ "$c" == "886" ]];then
 	service ss5 stop
-	rm -rf ss5-3.8.9
+	rm -rf /run/ss5
+ rm -f 	/run/lock/subsys/ss5
 	rm -rf /etc/opt/ss5
 	rm -f /usr/local/bin/s5
+ rm -rf 	/usr/lib/ss5
+rm -f /usr/sbin/ss5
+rm -rf /usr/share/doc/ss5
+rm -rf /root/ss5-3.8.9
+rm -f /etc/sysconfig/ss5
+rm -f /etc/rc.d/init.d/ss5
+rm -f /etc/pam.d/ss5
+rm -rf /var/log/ss5
 	clear
 	echo "Socks5服务卸载完毕！"
 	exit 0
 	else
-		bash /etc/opt/ss5/service.sh
+		bash $serviceFile
 	fi
 fi
