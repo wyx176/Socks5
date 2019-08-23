977aa7d61c287b3c08759005077c45b2d9d5bf54
17DL <32320103+wyx176@users.noreply.github.com>
2019-08-23 13:02:24 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 #Set PATH
 
+#Check OS
+if [ -n "$(grep 'Aliyun Linux release' /etc/issue)" -o -e /etc/redhat-release ];then
+OS=CentOS
+[ -n "$(grep ' 7\.' /etc/redhat-release)" ] && CentOS_RHEL_version=7
+[ -n "$(grep ' 6\.' /etc/redhat-release)" -o -n "$(grep 'Aliyun Linux release6 15' /etc/issue)" ] && CentOS_RHEL_version=6
+[ -n "$(grep ' 5\.' /etc/redhat-release)" -o -n "$(grep 'Aliyun Linux release5' /etc/issue)" ] && CentOS_RHEL_version=5
+elif [ -n "$(grep 'Amazon Linux AMI release' /etc/issue)" -o -e /etc/system-release ];then
+OS=CentOS
+CentOS_RHEL_version=6
+elif [ -n "$(grep bian /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Debian' ];then
+OS=Debian
+[ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
+Debian_version=$(lsb_release -sr | awk -F. '{print $1}')
+elif [ -n "$(grep Deepin /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Deepin' ];then
+OS=Debian
+[ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
+Debian_version=$(lsb_release -sr | awk -F. '{print $1}')
+elif [ -n "$(grep Ubuntu /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Ubuntu' -o -n "$(grep 'Linux Mint' /etc/issue)" ];then
+OS=Ubuntu
+[ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
+Ubuntu_version=$(lsb_release -sr | awk -F. '{print $1}')
+[ -n "$(grep 'Linux Mint 18' /etc/issue)" ] && Ubuntu_version=16
+elif [ ! -z "$(grep 'Arch Linux' /etc/issue)" ];then
+    OS=Ubuntu
+else
+echo "Does not support this OS, Please contact the author! "
+kill -9 $$
+fi
+
 resName="wyx176"
 serviceFile=/etc/opt/ss5/service.sh
+unIptablesFile=/etc/opt/ss5/unIptables.sh
 newVersionMsg=`curl -s -L  https://raw.githubusercontent.com/${resName}/Socks5/master/update.txt`
 
 echo ""
@@ -92,6 +122,14 @@ rm -f /etc/sysconfig/ss5
 rm -f /etc/rc.d/init.d/ss5
 rm -f /etc/pam.d/ss5
 rm -rf /var/log/ss5
+bash $unIptablesFile
+	if [[ $CentOS_RHEL_version == 7 ]];then
+	Iptab=`service iptables save`
+		systemctl restart iptables.service
+	else
+	Iptab=`/etc/init.d/iptables save`
+		/etc/init.d/iptables restart
+	fi
 	clear
 	echo "Socks5服务卸载完毕！"
 	exit 0
