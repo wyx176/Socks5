2f9ab735eb2393f3113e58ccfdfa02d0dcf1fad5
17DL <32320103+wyx176@users.noreply.github.com>
2019-08-23 14:37:28 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -67,7 +67,27 @@ source ~/.bash_profile
 #关闭防火墙
 newVersion=`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`
 if [[ ${newVersion} = "7" ]] ; then
-	systemctl stop firewalld.service
+	 systemctl stop firewalld.service
+    yum install iptables-services -y
+    sshport=$(netstat -nlp | grep sshd | awk '{print $4}' | awk -F : '{print $NF}' | sort -n | uniq)
+    cat << EOF > /etc/sysconfig/iptables
+# sample configuration for iptables service
+# you can edit this manually or use system-config-firewall
+# please do not ask us to add additional ports/services to this default configuration
+*filter
+:INPUT ACCEPT [0:0]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A INPUT -p icmp -j ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A INPUT -p tcp -m state --state NEW -m tcp --dport ${sshport} -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+-A INPUT -j REJECT --reject-with icmp-host-prohibited
+-A FORWARD -j REJECT --reject-with icmp-host-prohibited
+COMMIT
+EOF
     systemctl restart iptables.service
     systemctl enable iptables.service
     systemctl disable firewalld.service
